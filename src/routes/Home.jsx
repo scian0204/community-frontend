018af612a69b31887614c43d7f2ca870673edfaa
scian0204 <scian0204@gmail.com>
2023-05-29 19:12:06 +0900

    Change page to refresh on URL change
    URL변경 시 페이지 새로고침 되도록 변경

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PostList from '../components/PostList';
 import BoardList from '../components/BoardList';
 import Login from '../components/Login';
 import { useParams } from 'react-router-dom';
+import UserContext from '../context/UserContext';
+import RightMenu from '../components/RightMenu';
 
 function Home(props) {
   const [boardId, setBoardId] = useState(useParams().boardId);
+  const param = useParams().boardId;
+  const { userData } = useContext(UserContext);
+
+  useEffect(() => {
+    setBoardId(param);
+  }, [useParams()]);
+
   return (
     <div>
       <br />
       <div className="container-fluid">
         <div className="row">
           <div className="col-sm-2">
-            <BoardList setBoardId={setBoardId} />
+            <BoardList boardId={boardId} setBoardId={setBoardId} />
           </div>
           <div className="col-sm-8">
             <PostList boardId={boardId} />
           </div>
           <div className="col-sm-2">
-            <Login />
+            {userData == null ? <Login /> : <RightMenu />}
           </div>
         </div>
       </div>
